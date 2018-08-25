2bdd2045d5ac61cccf87cb5cf917f77e409f2fbe
fattanafif <fattanafifr@gmail.com>
2018-08-26 02:05:30 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+##############################################################################################################
+$config['urls'] = 'hUDJSNChdiej73dosN8S7KcyJneEdcrypto2'; //from urls
+$config['sessid'] = '4fq6virf9vjaov2cskimu63e76'; //from http cookie PHPSESSID headers
+$config['worker'] = '50'; //choose wisely, be careful, too much worker causing banned account
+##############################################################################################################
